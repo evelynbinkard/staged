ffafec67fa8af0b53aac38e6a8b377639c15a430
Evelyn Binkard <evelyn.binkard@gmail.com>
2019-08-07 19:57:02 -0500

    added dropdown menu to log in and conditionally rendered Options

--- staged/components/Login.js
+++ staged/components/Login.js
@@ -1,15 +1,45 @@
 import React from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
+import DropdownMenu from 'react-native-dropdown-menu';
 
 
 export default class Login extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            text: ''
+        }
+      
+    }
+
     render () {
+    var data = [["producer", "designer", "model", "backstage"]];
     return (
       <View style={styles.container}>
-        <Text>Please enter your name, password and role</Text>
+       
         <TextInput placeholder="name"/>
         <TextInput placeholder="password" />
-        
+        <DropdownMenu
+          style={{flex: 1}}
+          bgColor={'white'}
+          tintColor={'#666666'}
+          activityTintColor={'green'}
+          maxHeight={300} 
+          handler={(selection, row) => this.setState({text: data[selection][row]})}
+          data={data}
+        >
+
+          <View style={{flex: 1}}>
+            <Text>Please enter your name, password and role</Text>
+          </View>
+
+        </DropdownMenu>
+        <Button title="Enter"
+            onPress={
+                () => this.props.navigation.navigate('Options', {
+                    role: this.state.text
+                })
+            }/>
       </View>
     );
   }
--- staged/components/Options.js
+++ staged/components/Options.js
@@ -1,15 +1,50 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator} from 'react-navigation';
 
 
 export default class Options extends React.Component {
+    
     render () {
-    return (
-      <View style={styles.container}>
-        <Text>welcome to options page</Text>
-        
-      </View>
-    );
+    const role = this.props.navigation.getParam('role', 'no role designated');
+    
+    if (role === 'model'){
+        return (
+            <View style={styles.container}>
+                <Text>Welcome!</Text>
+                <Text>Your call time is:</Text>
+                <Text>You are walking for the following designers:</Text>
+            </View>
+        );
+    }
+    if (role === 'producer'){
+        return (
+            <View style={styles.container}>
+                <Text>Welcome! Would you like to:</Text>
+                <Text>Check in cast</Text>
+                <Text>See a show summary</Text>
+            </View>
+        );
+    }
+    if (role === 'designer'){
+        return (
+            <View style={styles.container}>
+                <Text>Welcome! Would you like to:</Text>
+                <Text>View assigned models for your collections</Text>
+                <Text>View tearsheet</Text>
+                <Text>Double check your playlists for your scenes</Text>
+            </View>
+        );
+    }
+    if (role === 'backstage') {
+        return (
+            <View style={styles.container}>
+                <Text>Welcome!</Text>
+                <Text>You're assigned to 'model'. S/he is/not checked in.</Text>
+                <Text>You will be working with the following designers/collections</Text>
+            </View>
+        );
+    }
   }
 }
 
